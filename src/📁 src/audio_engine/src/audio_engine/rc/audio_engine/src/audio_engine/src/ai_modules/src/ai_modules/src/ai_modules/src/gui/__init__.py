38ec7e5a1f8c0aa9796d4626e90c41520f45093c
Britbrat22 <131219149+Britbrat22@users.noreply.github.com>
2025-12-25 04:38:46 -1000

    Create __init__.py

---
+++ src/📁 src/audio_engine/src/audio_engine/rc/audio_engine/src/audio_engine/src/ai_modules/src/ai_modules/src/ai_modules/src/gui/__init__.py
@@ -0,0 +1,7 @@
+# GUI Package
+from .main_window import MainWindow
+from .track_widget import TrackWidget
+from .effects_panel import EffectsPanel
+from .export_dialog import ExportDialog
+
+__all__ = ['MainWindow', 'TrackWidget', 'EffectsPanel', 'ExportDialog']
